8d5c2b8c0210f7a91941e4d4018f3ffd9487f684
Mahir Oberai <49322365+MahirOberai@users.noreply.github.com>
2021-03-07 18:05:47 +0800

    fixed index match

--- historical_data.py
+++ historical_data.py
@@ -6,16 +6,21 @@ import datetime
 import pytz
 import numpy as np
 import matplotlib.pylab as plt
+from sklearn.preprocessing import MinMaxScaler
 #from broadwaybet import start_time, end_time
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.ensemble import RandomForestRegressor
+#from sklearn.model_selection import train_test_split
+#from sklearn import metrics
 
 #start_time = 01
 
 start = '01-24-2020'
 end = '01-24-2021'
+
+sentiment_df = pd.read_csv('test_quant.csv')
+sentiment_df.index = pd.to_datetime(sentiment_df['date'])
+sentiment_df.drop(columns=['date'])
 
 def get_ticker_data(start, end, ticker):
     """
@@ -30,9 +35,11 @@ def get_ticker_data(start, end, ticker):
         A pandas dataframe with the stock data.
     """
     try:
+        print('Getting ticker historical data from IEX')
         data = pdr.DataReader(ticker, 'iex', start, end)
         data.index = pd.to_datetime(data.index)
     except:
+        print('Getting ticker historical data from Yahoo')
         data = pdr.get_data_yahoo(ticker, start, end)
     return data 
 
@@ -43,35 +50,116 @@ tsla_historicals['5_SMA'] = tsla_historicals['Close'].rolling(window=5).mean()
 
 tsla_historicals = tsla_historicals[tsla_historicals['5_SMA'].notna()]
 
+sentiment_df = sentiment_df[sentiment_df.index.isin(tsla_historicals.index)]
+
+tsla_historicals = tsla_historicals[tsla_historicals.index.isin(tsla_historicals.index.intersection(sentiment_df.index))]
+
+tsla_historicals.to_csv('tsla_historicals_test.csv')
+sentiment_df.to_csv('sentiment_df_test.csv')
+
+print(len(tsla_historicals))
+print(len(sentiment_df))
+
+close_fft = np.fft.fft(np.asarray(sentiment_df['score'].tolist()))
+fft_df = pd.DataFrame({'fft':close_fft})
+fft_df['absolute'] = fft_df['fft'].apply(lambda x: np.abs(x))
+fft_df['angle'] = fft_df['fft'].apply(lambda x: np.angle(x))
+fft_list = np.asarray(fft_df['fft'].tolist())
+
+for num_ in [5, 10, 15, 20]:
+    fft_list_m10= np.copy(fft_list); fft_list_m10[num_:-num_]=0
+    sentiment_df['fourier '+str(num_)]=np.fft.ifft(fft_list_m10)
+
+sc= MinMaxScaler(feature_range=(0,1))
+norm_df = pd.DataFrame(index=tsla_historicals.index)
+norm_df['norm_price']=sc.fit_transform(tsla_historicals['Close'].to_numpy().reshape(-1, 1))
+norm_df['price_log']=np.log(tsla_historicals['Close']/tsla_historicals['Close'].shift(1))
+norm_df['norm_sentiment']=sc.fit_transform(sentiment_df['score'].to_numpy().reshape(-1, 1))
+norm_df['norm_fourier5']=sc.fit_transform(np.asarray(list([(float(x)) for x in sentiment_df['fourier 5'].to_numpy()])).reshape(-1, 1))
+norm_df['norm_fourier10']=sc.fit_transform(np.asarray(list([(float(x)) for x in sentiment_df['fourier 10'].to_numpy()])).reshape(-1, 1))
+norm_df['norm_fourier15']=sc.fit_transform(np.asarray(list([(float(x)) for x in sentiment_df['fourier 15'].to_numpy()])).reshape(-1, 1))
+norm_df['norm_fourier20']=sc.fit_transform(np.asarray(list([(float(x)) for x in sentiment_df['fourier 20'].to_numpy()])).reshape(-1, 1))
+
 # SMA trade calls
-buys=[]
-sells=[]
+sma_buys=[]
+sma_sells=[]
 for i in range(len(tsla_historicals)-1):
     if ((tsla_historicals['2_SMA'].values[i] < tsla_historicals['5_SMA'].values[i]) 
     & (tsla_historicals['2_SMA'].values[i+1] > tsla_historicals['5_SMA'].values[i+1])):
-        print("Trade Call for {row} is Buy.".format(row=tsla_historicals.index[i].date()))
-        buys.append(i)
+        #print("Trade Call for {row} is Buy.".format(row=tsla_historicals.index[i].date()))
+        sma_buys.append(i)
     elif ((tsla_historicals['2_SMA'].values[i] > tsla_historicals['5_SMA'].values[i]) & (tsla_historicals['2_SMA'].values[i+1] < tsla_historicals['5_SMA'].values[i+1])):
-        print("Trade Call for {row} is Sell.".format(row=tsla_historicals.index[i].date()))
-        sells.append(i)
+        #print("Trade Call for {row} is Sell.".format(row=tsla_historicals.index[i].date()))
+        sma_sells.append(i)
 
-profit_gross = sum(buys) - sum(sells)
-profit_percent = round((sum(buys) - sum(sells)) / sum(buys) * 100, 2)
-print("Profit for period {start} to {end} using SMA strategy is ${profit_gross} / {profit_percent}%."
-        .format(start=start, end=end, profit_gross=profit_gross, profit_percent=profit_percent))
+sma_profit_gross = 0
+for i in range(len(sma_buys)):
+    sma_profit_gross += sma_buys[i] - sma_sells[i]
 
-plt.figure(figsize=(20, 10),dpi=80)
-plt.plot(tsla_historicals.index, tsla_historicals['Close'])
-plt.plot(tsla_historicals.index, tsla_historicals['2_SMA'],'-^', markevery=buys, ms=15, color='green')
-plt.plot(tsla_historicals.index, tsla_historicals['5_SMA'],'-v', markevery=sells, ms=15, color='red')
+#profit_gross = sum(buys) - sum(sells)
+sma_profit_percent = round((sum(sma_buys) - sum(sma_sells)) / sum(sma_buys) * 100, 2)
+#print("Profit for period {start} to {end} using SMA strategy is ${sma_profit_gross} / {sma_profit_percent}%."
+        #.format(start=start, end=end, sma_profit_gross=sma_profit_gross, sma_profit_percent=sma_profit_percent))
+
+# Sentiment SMA trade calls
+norm_df['2_SMA'] = norm_df['norm_fourier20'].rolling(window=2).mean()
+norm_df['5_SMA'] = norm_df['norm_fourier20'].rolling(window=5).mean()
+
+senti_sma_buys=[]
+senti_sma_sells=[]
+
+#norm_fourier20 = np.array(norm_df['norm_fourier20']).convolve()
+
+# Buy at local max sell at local min??
+
+#np.r_[True, a[1:] < a[:-1]] & np.r_[a[:-1] < a[1:], True]
+
+for i in range(len(tsla_historicals)-1):
+    if ((norm_df['2_SMA'].values[i] < norm_df['5_SMA'].values[i]) 
+    & (norm_df['2_SMA'].values[i+1] > norm_df['5_SMA'].values[i+1])):
+        #print("Trade Call for {row} is Buy.".format(row=norm_df.index[i].date()))
+        senti_sma_buys.append(tsla_historicals['Close'].values[i])
+    elif ((norm_df['2_SMA'].values[i] > norm_df['5_SMA'].values[i]) & (norm_df['2_SMA'].values[i+1] < norm_df['5_SMA'].values[i+1])):
+        #print("Trade Call for {row} is Sell.".format(row=norm_df.index[i].date()))
+        senti_sma_sells.append(tsla_historicals['Close'].values[i])
+
+senti_sma_profit_gross = 0
+for i in range(len(senti_sma_buys)):
+    senti_sma_profit_gross += senti_sma_buys[i] - senti_sma_sells[i]
+
+#profit_gross = sum(buys) - sum(sells)
+senti_sma_profit_percent = round((sum(senti_sma_buys) - sum(senti_sma_sells)) / sum(senti_sma_buys) * 100, 2)
+#print("Profit for period {start} to {end} using Sentiment SMA strategy is ${senti_sma_profit_gross} / {senti_sma_profit_percent}%."
+        #.format(start=start, end=end, senti_sma_profit_gross=senti_sma_profit_gross, senti_sma_profit_percent=senti_sma_profit_percent))
+
+fig = plt.figure(figsize=(20, 10),dpi=80)
+ax1 = fig.add_subplot(111)
+#ax1.plot(tsla_historicals.index, tsla_historicals['Close'])
+#ax1.plot(tsla_historicals.index, tsla_historicals['2_SMA'],'-^', markevery=sma_buys, ms=15, color='green')
+#ax1.plot(tsla_historicals.index, tsla_historicals['5_SMA'],'-v', markevery=sma_sells, ms=15, color='red')
+ax2 = ax1.twinx()
+#ax2.plot(norm_df.index, sentiment_df['norm_price'],'-^', markevery=sma_buys, ms=15, color='green')
+#ax2.plot(norm_df.index, norm_df['5_SMA'],'-v', markevery=sma_sells, ms=15, color='red')
+ax1.plot(tsla_historicals.index, norm_df['norm_price'], ms=15)
+#ax2.plot(tsla_historicals.index, norm_df['norm_sentiment'], ms=15, color='purple')
+#ax2.plot(tsla_historicals.index, norm_df['norm_fourier5'], ms=15)
+#ax2.plot(tsla_historicals.index, norm_df['norm_fourier10'], ms=15)
+#ax2.plot(tsla_historicals.index, norm_df['norm_fourier15'], ms=15)
+ax1.plot(tsla_historicals.index, norm_df['norm_fourier20'], ms=15, color='purple')
+#ax2.legend(['norm_price','norm_sentiment','norm_fourier5','norm_fourier10', 'norm_fourier15', 'norm_fourier20'])
+#ax2.legend(['norm_price','norm_fourier20'])
 plt.xlabel('Date',fontsize=14)
-plt.ylabel('Price in Dollars', fontsize = 14)
+#plt.ylabel('Price in Dollars', fontsize = 14)
+plt.ylabel('Price in Dollars and Sentiment Score', fontsize = 14)
 plt.xticks(rotation='60',fontsize=12)
 plt.yticks(fontsize=12)
-plt.title('Trade Calls - Moving Averages Crossover - ' + str(profit_percent) + '%' , fontsize = 16)
-plt.legend(['Close','2_SMA','5_SMA'])
+#plt.title('Normalized Sentiment Fourier - Trade Calls - Moving Averages Crossover - ' + str(sma_profit_percent) + '%' , fontsize = 16)
+plt.title('TSLA r/wallstreetbets Sentiment Analysis 1/24/2020-1/24/2021 (normalized)', fontsize = 16)
+ax1.legend(['norm_price','norm_fourier20'])
+#ax2.legend(['norm_2_SMA', 'norm_5_SMA'])
 plt.grid()
-plt.savefig('TSLA_SMA')
+#plt.savefig('TSLA_SMA')
+plt.savefig('TSLA_Sentiment and Price (normalized)2')
 #plt.show() 
 
 
--- sentiment.py
+++ sentiment.py
@@ -56,35 +56,44 @@ def get_sentiment_analyzer():
 def get_nltk_sentiment(analyzer, blob):
   vs = analyzer.polarity_scores(blob)
 
-  if not vs['neg'] > 0.05:
-    if vs['pos'] - vs['neg'] > 0:
-      return Sentiment.POSITIVE
-    else:
-      return Sentiment.NEUTRAL
+  return vs['compound']
+  # if not vs['neg'] > 0.05:
+  #   if vs['pos'] - vs['neg'] > 0:
+  #     return Sentiment.POSITIVE
+  #   else:
+  #     return Sentiment.NEUTRAL
 
-  elif not vs['pos'] > 0.05:
-    if vs['pos'] - vs['neg'] <= 0:
-      return Sentiment.NEGATIVE
-    else:
-      return Sentiment.NEUTRAL
-  else:
-    return Sentiment.NEUTRAL
+  # elif not vs['pos'] > 0.05:
+  #   if vs['pos'] - vs['neg'] <= 0:
+  #     return Sentiment.NEGATIVE
+  #   else:
+  #     return Sentiment.NEUTRAL
+  # else:
+  #   return Sentiment.NEUTRAL
 
 def replies_of(analyzer, top_level_comment):
   if len(top_level_comment.replies) == 0:
-    return []
+    return 0, 0
   else:
-    sentiments = []
+    #sentiments = []
 
+    count = 0
+    avg_sentiment = 0
     for num, comment in enumerate(top_level_comment.replies):
-          try:
-              comment_sentiment = get_nltk_sentiment(analyzer, comment.body)
-              sentiments.append(comment_sentiment)
-          except:
-              continue
-          sentiments.append(replies_of(analyzer, comment))
+      try:
+          count += 1
+          #comment_sentiment = get_nltk_sentiment(analyzer, comment.body)
+          avg_sentiment += get_nltk_sentiment(analyzer, comment.body)
+          #sentiments.append(comment_sentiment)
+      except:
+          continue
+      sub_sentiment, sub_count = replies_of(analyzer, comment)
+      avg_sentiment += sub_sentiment
+      count += sub_count
 
-    return sentiments
+    return avg_sentiment, count
+
+
 
 def calculate_date_sentiments(analyzer, api, posts_list, analyze_comments):
   pos = 0
@@ -92,6 +101,7 @@ def calculate_date_sentiments(analyzer, api, posts_list, analyze_comments):
   date_sentiments = {}
 
   for post in posts_list:
+    title_sentiment = get_nltk_sentiment(analyzer, post.title)
     date_sentiments.setdefault(post.created, 0)
 
     if analyze_comments:
@@ -99,29 +109,55 @@ def calculate_date_sentiments(analyzer, api, posts_list, analyze_comments):
 
       for count, top_level_comment in enumerate(req.comments):
         try:
-            top_comment_sentiment = get_nltk_sentiment(analyzer, top_level_comment.body)
-            comment_sentiments = replies_of(analyzer, top_level_comment)
-            comment_sentiments.append(top_comment_sentiment)
-            for comment_sentiment in comment_sentiments:
-              if (comment_sentiment == Sentiment.POSITIVE):
-                pos += 1
-                #date_sentiments[post.created] = date_sentiments[post.created] + 1
-                
-              elif (comment_sentiment == Sentiment.NEGATIVE):
-                neg += 1
-                #date_sentiments[post.created] = date_sentiments[post.created] - 1
+          top_comment_sentiment = get_nltk_sentiment(analyzer, top_level_comment.body)
+          replies_sentiment, num_total_sentiments = replies_of(analyzer, top_level_comment)
+          total_sentiment = title_sentiment + top_comment_sentiment + replies_sentiment
+          num_total_sentiments += 2
+          avg_sentiment = total_sentiment / num_total_sentiments
+
+          date_sentiments[post.created] = avg_sentiment
+        #comment_sentiments.append(top_comment_sentiment)
+        #comment_sentiments.append(title_sentiment)
+        
+        #for comment_sentiment in comment_sentiments:
+            #print('comment: ', comment_sentiment)
+  
+
+            #print(comment_sentiment)
+            #print(sum(comment_sentiment)/len(comment_sentiment))
+            #date_sentiments[post.created] += sum(comment_sentiment)/len(comment_sentiment)
+                #date_sentiments[post.created] = date_sentiments[post.created] +
+                #print(analyzer.polarity_scores(comment_sentiment))
+                # if (comment_sentiment == Sentiment.POSITIVE):
+                #   pos += Sentiment.POSITIVE
+                #   date_sentiments[post.created] = date_sentiments[post.created] + Sentiment.POSITIVE
+                #   print(comment_sentiment)
+                #   #date_sentiments[post.created] = date_sentiments[post.created] + 1
+                  
+                # elif (comment_sentiment == Sentiment.NEGATIVE):
+                #   neg += 1
+                #   date_sentiments[post.created] = date_sentiments[post.created] + Sentiment.NEGATIVE
+                #   #date_sentiments[post.created] = date_sentiments[post.created] - 1
+        
+        #date_sentiments[post.created] = date_sentiments[post.created] / (len(comment_sentiments))
+        #print('date: ', date_sentiment[post.created])
         except:
-            continue
+          continue
 
-    title_sentiment = get_nltk_sentiment(analyzer, post.title)
+        
+    
+    else:
+      date_sentiments[post.created] = title_sentiment
+      #title_sentiment = get_nltk_sentiment(analyzer, post.title)
+      
 
-    if (title_sentiment == Sentiment.POSITIVE):
-      pos += 1
-      #date_sentiments[post.created] = date_sentiments[post.created] + 1
-    elif (title_sentiment == Sentiment.NEGATIVE):
-      neg += 1
-      #date_sentiments[post.created] = date_sentiments[post.created] - 1
+    # if (title_sentiment == Sentiment.POSITIVE):
+    #   pos += 1
+    #   #date_sentiments[post.created] = date_sentiments[post.created] + 1
+    # elif (title_sentiment == Sentiment.NEGATIVE):
+    #   neg += 1
+    #   #date_sentiments[post.created] = date_sentiments[post.created] - 1
 
-    date_sentiments[post.created] = pos/neg
+    # date_sentiments[post.created] = pos/neg
 
   return date_sentiments
